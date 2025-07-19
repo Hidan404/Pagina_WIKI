da5565f717ecd86e23c7dab68d808c7a49b40384
Ronald Antonio Oliveira de sousa <ronaldkurouzo@gmail.com>
2025-07-18 22:01:43 -0300

    feat: implementar formulário de usuário e rotas para páginas

--- App/formulario.py
+++ App/formulario.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from wtforms import FlaskForm, StringField, SubmitField
 from wtforms.validators import DataRequired
+from App.models import User, db
 
 
 class UserForm(FlaskForm):
@@ -10,3 +11,13 @@ class UserForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired()])
     password = StringField('Password', validators=[DataRequired()])
     submit = SubmitField('Sign Up')
+    
+    def save_user(self):
+        user = User(
+            username=self.username.data,
+            email=self.email.data,
+            password=self.password.data
+        )
+        db.session.add(user)
+        db.session.commit()
+        return user
--- App/router.py
+++ App/router.py
@@ -4,3 +4,11 @@ from flask import render_template, redirect, url_for
 @app.route('/')
 def home():
     return render_template('base.html')
+
+@app.route("/home", methods=['GET'])
+def home_page():
+    return render_template('home.html')
+
+@app.route("/wikis", methods=['GET'])
+def wikis_page():
+    return render_template('wikis.html')
