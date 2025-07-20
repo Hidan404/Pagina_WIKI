b458d494d91be4aab9d8b7cb5c4de097138044ba
Ronald Antonio Oliveira de sousa <ronaldkurouzo@gmail.com>
2025-07-19 21:48:24 -0300

    feat: adicionar rotas para detalhes de wikis e comentários, atualizar navegação

--- App/models.py
+++ App/models.py
@@ -11,3 +11,19 @@ class User(db.Model):
 
     def __repr__(self):
         return f"User('{self.username}', '{self.email}', '{self.date_joined}')"
+    
+    
+    def save_bd(self):
+        db.session.add(self)
+        
+        
+class Comentario(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    conteudo = db.Column(db.Text, nullable=False)
+    data_criacao = db.Column(db.DateTime, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    user = db.relationship('User', backref=db.backref('comentarios', lazy=True))
+
+    def __repr__(self):
+        return f"Comentario('{self.user.username}', '{self.data_criacao}')"        
--- App/router.py
+++ App/router.py
@@ -1,6 +1,7 @@
 from App import app
 from flask import render_template, redirect, url_for
-
+from App.models import User, db, Comentario
+ 
 @app.route('/')
 def home():
     return render_template('base.html')
@@ -12,3 +13,13 @@ def home_page():
 @app.route("/wikis", methods=['GET'])
 def wikis_page():
     return render_template('wikis.html')
+
+@app.route("/wiki_detalhes", methods=["GET"])
+def all_wikis():
+    return render_template('wiki_detalhes.html')
+
+
+@app.route("/comentar", methods=["POST"])
+def comentario():
+    comentar = Comentario.query.order_by(Comentario.data_criacao.desc()).limit(5).all()
+    return render_template('home.html', comentar=comentar)
